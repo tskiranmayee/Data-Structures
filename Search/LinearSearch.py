a650761354d617a8b245cb422c4aa32a15ea34dd
Tadepalli Sarada <tskiranmayee@gmail.com>
2021-05-10 20:46:36 -0500

    Data Structures folder created

---
+++ Search/LinearSearch.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 19:53:21 2021
+
+@author: tskir
+"""
+
+""" Linear Search-Easy way"""
+a=[4,3,5,2,6]
+q=a.index(5)
+print("Index of 5 is:",q)
---
+++ Search/LinearSearch2.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 19:58:33 2021
+
+@author: tskiranmayee
+"""
+
+"""Linear Search-The Hard way
+Best case:O(1)    - found at first location
+Average case:O(n) - found at the middle
+Best case:O(n)    -   """
+found=False
+q=8
+a=[4,5,7,8,1,3]
+for i in range(len(a)):
+    if(q==a[i]):
+        found=True
+        break
+    else:
+        i=i+1
+        
+if(found==True):
+     print("Found {} at index {} in array {}".format(q,i,a))
+else:
+     print("Not Found {} in the array {}".format(q,a))
+     
+     
+
+        
+    
+
+
+
