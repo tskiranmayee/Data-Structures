0be6fc64d47226944bd9af79a76687eea72cac7b
Tadepalli Sarada <tskiranmayee@gmail.com>
2021-05-25 21:39:54 -0500

    Duplicate found or not - added

--- Arrays/DuplicatesInArray.py
+++ Arrays/DuplicatesInArray.py
@@ -3,14 +3,15 @@ def duplicateOrNot(arr):
     for i in arr:
         if(arr[abs(i)]>=0):
             arr[abs(i)]=-arr[abs(i)]
+
         else:
+            print("Duplicate found for {}".format(abs(i)))
             return True
 
 
-
-arr=[1,2,0,3]
+arr=[1,2,2,3]
 f=duplicateOrNot(arr)
 if(f==True):
-    print("Duplicate found")
+    pass
 else:
     print("Duplicate not found")
